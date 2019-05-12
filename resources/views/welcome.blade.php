71562f407bc3e9e59f75fa77b1e1d2a486b62679
Anargya Widyadhana <anargyawidyadhana@gmail.com>
2019-05-12 15:00:30 +0700

    normal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,7 +64,6 @@
         </style>
     </head>
     <body>
-        a
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
